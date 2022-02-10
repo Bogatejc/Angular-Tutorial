52de9d2fcb073a6afe11a0ed5ba8afe281ebe07b
Bogatejc <mark.bogataj18@gmail.com>
2022-02-10 09:53:17 +0100

    Adding code from first week

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Ingredient} from "../shared/ingredient.model";
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+	ingredients: Ingredient[] = [
+		new Ingredient("Tomatoes", 3),
+		new Ingredient("Potatoes", 5),
+		new Ingredient("Watermelons", 12)
+	];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
